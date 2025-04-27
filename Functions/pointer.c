0522f5b9607192dd71fd47a8210721109b05da6f
Aditya Codes <techaditya36@gmail.com>
2025-04-27 21:14:21 +0530

    Add files via upload

---
+++ Functions/pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 25;
+    int* x = &a; // int* -> int ka address store karta hai
+    // VVIP -> *x = 7; // a is changed.
+    int** y = &x; // int** -> int* ka address store karta hai
+    int*** z = &y;
+    printf("%d\n", a);
+    printf("%d\n", *x); // %p se address print hota hai
+    printf("%d\n", **y); 
+    printf("%d\n", ***z); 
+
+
+    return 0;
+}
